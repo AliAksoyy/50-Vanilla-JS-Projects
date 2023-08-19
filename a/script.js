4cecbd3d865ef485f7dfced1b4eeb36234843194
AliAksoyy <alifeyza4017@gmail.com>
2023-08-19 13:40:30 +0300

    add

--- a/script.js
+++ a/script.js
@@ -1,25 +1,14 @@
-const users = [
-  { firstName: "Ali", lastName: "Aksoy", age: 33 },
-  { firstName: "Beyda", lastName: "Aksoy", age: 4 },
-  { firstName: "Feyza", lastName: "Aksoy", age: 30 },
-  { firstName: "Mustafa", lastName: "Aksoy", age: 30 },
-  { firstName: "AhmetIsa", lastName: "Aksoy", age: 33 },
-];
+console.log("s");
 
-const se = users.reduce((acc, cur) => {
-  console.log("acc", acc);
-  console.log("cur", cur);
-  const newAcc = acc;
-  const { age } = cur;
-  console.log(age);
-  if (!acc[age]) {
-    console.log(acc[age]);
-    return { ...newAcc, [age]: 1 };
-  } else {
-    console.log("object");
+setTimeout(() => {
+  console.log("callback");
+}, 5000);
 
-    return { ...newAcc, [age]: ++newAcc[age] };
-  }
-}, {});
+let startDate = new Date().getTime();
+let endDate = startDate;
 
-console.log("se", se);
+while (endDate < startDate + 10000) {
+  endDate = new Date().getTime();
+}
+
+console.log("while expires");
