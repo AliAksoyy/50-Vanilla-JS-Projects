a5d54ba646d1cb25db477d5156b6c87d8cddaf33
AliAksoyy <alifeyza4017@gmail.com>
2023-08-25 20:20:20 +0300

    #32 drink water page yapildi

--- 16-Drink-Water/script.js
+++ 16-Drink-Water/script.js
@@ -4,7 +4,6 @@ const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
 smallCups.forEach((cup, index) => {
-  console.log(index);
   cup.addEventListener("click", () => higlightCups(index));
 });
 
@@ -13,13 +12,36 @@ function higlightCups(index) {
     smallCups[index].classList.contains("full") &&
     !smallCups[index].nextElementSibling.classList.contains("full")
   ) {
-    index--
+    index--;
   }
   smallCups.forEach((cup, i) => {
-    if (i < index) {
+    if (i <= index) {
       cup.classList.add("full");
     } else {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCup = document.querySelectorAll(".cup-small.full")?.length;
+  const totalCups = smallCups?.length;
+
+  if (fullCup === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCup / totalCups) * 330}px`;
+    percentage.innerText = `%${(fullCup / totalCups) * 100}`;
+  }
+  if (fullCup === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2- ((250 * fullCup) / 1000)}`;
+  }
 }
